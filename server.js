aef267b3f2ce71fdadf2af0f1390adb3181d8032
Andi Weber <adtweber@gmail.com>
2024-04-23 17:45:42 -0400

    create and new routes completed

--- server.js
+++ server.js
@@ -1,38 +1,32 @@
 // DEPENDENCIES
 const express = require('express')
 
-
 // CONFIGURATION
 require('dotenv').config()
 const PORT = process.env.PORT
 const app = express()
 
-
 // MIDDLEWARE
 app.set('views', __dirname + '/views')
 app.set('view engine', 'jsx')
 app.engine('jsx', require('express-react-views').createEngine())
+app.use(express.urlencoded({ extended: true }))
 app.use(express.static('public'))
-
 
 // ROUTES
 app.get('/', (req, res) => {
-    res.send('Welcome to an Awesome App about Breads!')
+    res.send('<h1>Welcome to an Awesome App about Breads!</h1>')
 })
-
 
 // Breads
 const breadsController = require('./controllers/breads_controller.js')
-app.use('/bread', breadsController)
+app.use('/breads', breadsController)
 
-
-// 404 Page
 app.get('*', (req, res) => {
-    res.send('404')
+    res.send('<h1>404 NOT FOUND</h1>')
 })
-
 
 // LISTEN
 app.listen(PORT, () => {
-    console.log('listening on port', PORT);
+    console.log('nomming at port', PORT);
 })
--- views/index.jsx
+++ views/index.jsx
@@ -1,21 +1,26 @@
-const React = require('react');
-const Default = require('./layouts/Default');
-function Index({ breads }) {
+const React = require('react')
+const Default = require('./layouts/Default')
+
+function Index({ breads, title }) {
     return (
-        <Default>
+        <Default title={title}>
             <h2>Index Page</h2>
-            {/* <p>I have {breads[0].name} bread!</p> */}
-            {/* This is a JSX comment. */}
             <ul>
-                {breads.map((bread, index) => {
-                    return (
-                        <li key={index}>
-                            <a href={`/bread/${index}`}>{bread.name}</a>
-                        </li>
-                    );
-                })}
+                {
+                    breads.map((bread, index) => {
+                        return (<li key={index}>
+                            <a href={`/breads/${index}`}>
+                                {bread.name}
+                            </a>
+                        </li>)
+                    })
+                }
             </ul>
+            <div className="newButton">
+                <a href="/breads/new"><button>Add a new bread</button></a>
+            </div>
         </Default>
-    );
+    )
 }
-module.exports = Index;
+
+module.exports = Index
